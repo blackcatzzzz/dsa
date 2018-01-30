fd1f4b7ee97aee3d92f8b00bb476e12f41f80840
zhangchi <zhangchi@zhihuisystem.com>
2018-01-30 17:59:28 +0800

    add comments for FindKthToTail

--- java/src/main/java/com/zhangchi/dsa/FindKthToTail.java
+++ java/src/main/java/com/zhangchi/dsa/FindKthToTail.java
@@ -16,13 +16,22 @@ public class FindKthToTail {
         }
     }
 
+    /**
+     * 思路：
+     * 1）假设有n的节点，那么要找到倒数第K个节点，需从头结点开始连续走n-k步。例如：1->2->3->4->5, n=5,k=2.连续走n-k=3步。
+     * 2）fast先走k步，此时fast走到尾部哨兵节点剩下n-k步
+     * 3）slow指向head,slow和fast同时走，当fast走到尾部哨兵节点时，slow走了n-k步，即指向了倒数k个节点
+     * @param head
+     * @param k
+     * @return
+     */
     public static ListNode findKthToTail(ListNode head,int k) {
         if (head == null || k == 0)
             return null;
 
         ListNode fast = head;
         while (k-- > 0){
-            if (fast == null) // 非法判断
+            if (fast == null) // 非法判断，k大于链表长度
                 return null;
             fast = fast.next;
         }
