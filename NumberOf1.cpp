79ee14e1ff7714bf025c2b94eaff7822126cd0cd
blackcatzzzz <504841840@qq.com>
2018-06-15 11:42:25 +0800

    add NumberOf1.cpp

---
+++ NumberOf1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	int  NumberOf1(int n) {
+		int count = 0;
+		while (n)
+		{
+			count++;
+			n = (n - 1) & n;
+		}
+
+		return count;
+	}
+};
+
+int main()
+{
+	return 0;
+}
