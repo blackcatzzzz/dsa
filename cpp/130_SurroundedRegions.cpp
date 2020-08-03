821473e2b6ef5389b578e24b0d813500d864628f
zhangchi <zhangchi4@kingsoft.com>
2020-08-03 11:43:37 +0800

    add 130 SurroundedRegions

--- cpp/130_SurroundedRegions.cpp
+++ cpp/130_SurroundedRegions.cpp
@@ -127,10 +127,50 @@ public:
     };
 };
 
+// DFS
+class Solution_DFS {
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        if(m == 0) return;
+        int n = board[0].size();
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                bool isEdge = i == 0 || i == m - 1 || j == 0 || j == n - 1;
+                if(isEdge && board[i][j] == 'O')
+                    dfs(board, i, j);
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if(board[i][j] == '#')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+
+private:
+    void dfs(vector<vector<char>>& board, int i, int j){
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size()
+        || board[i][j] == 'X' || board[i][j] == '#' )
+            return;
+
+        board[i][j] = '#';
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+};
+
 
 int main(){
     vector<vector<char>> board{{'X','O','X','O','X','O'},{'O','X','O','X','X','X'},{'X','X','X','X','X','O'},{'O','X','O','X','O','X'}};
-    Solution S;
+    Solution_DFS S;
     S.solve(board);
     getchar();
 }
