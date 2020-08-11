3eef491c10e2efcdd14a64c3de807bad3069ebde
zhangchi <zhangchi4@kingsoft.com>
2020-08-11 20:55:09 +0800

    021_MergeTwoSortedLists.cpp

---
+++ cpp/021_MergeTwoSortedLists.cpp
@@ -0,0 +1,56 @@
+/*
+21. Merge Two Sorted Lists
+Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+
+#include "common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        // if(!l1 && !l2) return nullptr;
+        if(!l1) return l2;
+        if(!l2) return l1;
+        ListNode* head = nullptr;
+        if(l1->val <= l2->val){
+            head = l1;
+            l1 = l1->next;
+        }else{
+            head = l2;
+            l2 = l2->next;
+        }
+            
+        ListNode* cur = head;
+        while(l1 && l2){
+            if(l1->val <= l2->val){
+                cur->next = l1;
+                l1 = l1->next;
+            }else{
+                cur->next = l2;
+                l2 = l2->next;
+            }
+
+            cur = cur->next;
+        }
+
+        if(l1) cur->next = l1;
+        if(l2) cur->next = l2;
+
+        return head;
+    }
+};
