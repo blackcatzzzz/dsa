3ac7be0d8063a726a0a8f013f4b8d8ac518e0296
zhangchi <zhangchi4@kingsoft.com>
2020-08-26 20:57:41 +0800

    add 0079_Word_Search.cpp

--- cpp/0079_Word_Search.cpp
+++ cpp/0079_Word_Search.cpp
@@ -31,10 +31,57 @@ board and word consists only of lowercase and uppercase English letters.
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        
+        this->m = board.size();
+        this->n = board[0].size();
+        this->word = word;
+        used = vector<vector<bool>>(m, vector<bool>(n, false));
+        direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+             if(dfs(board, i, j, 0))
+                return true;
+        return false;
     }
+
+private:
+    bool dfs(vector<vector<char>>& board, int i, int j, int start){
+        if(board[i][j] != word[start])
+            return false;
+
+        if(start == word.size() - 1)
+            return board[i][j] == word[start];
+
+        used[i][j] = true;
+        for(auto d: direction){
+            int newX = i + d[0];
+            int newY = j + d[1];
+            if(newX < 0 || newX == m || newY < 0 || newY == n 
+                || used[newX][newY])
+                continue;
+            
+            if(dfs(board, newX, newY, start + 1))
+                return true;
+        }
+        used[i][j] = false;
+
+        return false;
+    }
+
+private:
+    vector<vector<bool>> used;
+    string word;
+    vector<vector<int>> direction;
+    int m;
+    int n;
 };
 
 int main(){
+    Solution S;
+    vector<vector<char>> board = {
+        {'A','B','C','E'},
+        {'S','F','C','S'},
+        {'A','D','E','E'}
+    };
 
+    cout << S.exist(board, "ABCCED");
 }
