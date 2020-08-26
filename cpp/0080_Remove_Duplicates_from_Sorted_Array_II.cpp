809a99b17e673acfd2431c98009b58ddd774f12d
zhangchi <zhangchi4@kingsoft.com>
2020-08-26 20:59:25 +0800

    init

--- cpp/0080_Remove_Duplicates_from_Sorted_Array_II.cpp
+++ cpp/0080_Remove_Duplicates_from_Sorted_Array_II.cpp
@@ -1,29 +1,39 @@
 /*
-79. Word Search
-Given a 2D board and a word, find if the word exists in the grid.
+80. Remove Duplicates from Sorted Array II
+Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
 
-The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-Example:
+Example 1:
 
-board =
-[
-  ['A','B','C','E'],
-  ['S','F','C','S'],
-  ['A','D','E','E']
-]
+Given nums = [1,1,1,2,2,3],
 
-Given word = "ABCCED", return true.
-Given word = "SEE", return true.
-Given word = "ABCB", return false.
- 
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
 
-Constraints:
+It doesn't matter what you leave beyond the returned length.
+Example 2:
 
-board and word consists only of lowercase and uppercase English letters.
-1 <= board.length <= 200
-1 <= board[i].length <= 200
-1 <= word.length <= 10^3
+Given nums = [0,0,1,1,1,1,2,3,3],
+
+Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
 */
 
 #include "common.h"
