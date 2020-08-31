d22d9f92b9cb805fd0624532d05e035ed2dbc425
zhangchi <zhangchi4@kingsoft.com>
2020-08-31 21:25:45 +0800

    add 0091_Decode_Ways.cpp

--- cpp/0091_Decode_Ways.cpp
+++ cpp/0091_Decode_Ways.cpp
@@ -25,11 +25,30 @@ Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 class Solution {
 public:
     int numDecodings(string s) {
-        
+        if(s.size() == 0) return 0;
+        vector<int> dp(s.size() + 1, 0); 
+        dp[0] = 1;
+        if(s[0] == '0') return 0;
+
+        dp[1] = 1;
+        for(int i = 2; i <= s.size(); i++){
+            dp[i] = (s[i - 1] != '0' ? dp[i - 1] : 0) + (isValid(s.substr(i - 2, 2)) ? dp[i - 2] : 0);
+        }
+            
+
+        return dp[s.size()];
+    }
+
+public:
+    bool isValid(const string& s){
+        int num = (s[1] - '0') + (s[0] - '0') * 10;
+        return num >= 10 && num <= 26;
     }
 };
 
 
 int main(){
-
+    string s = "226";
+    Solution S;
+    cout << S.numDecodings(s);
 }
