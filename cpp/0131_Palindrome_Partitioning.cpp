3a7676b9b108ec3b07a75369a0b4cc36db1cd7d5
zhangchi <zhangchi4@kingsoft.com>
2020-09-03 18:55:24 +0800

    init

---
+++ cpp/0131_Palindrome_Partitioning.cpp
@@ -0,0 +1,28 @@
+/*
+131. Palindrome Partitioning
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+Example:
+
+Input: "aab"
+Output:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+*/
+
+#include "common.h"
+
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+
+    }
+};
+
+int main(){
+
+}
