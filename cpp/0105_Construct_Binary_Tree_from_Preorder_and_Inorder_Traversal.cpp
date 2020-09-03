24dd3a2821fccd95bad6b3af1daf3f7c83f358e3
zhangchi <zhangchi4@kingsoft.com>
2020-09-03 12:05:44 +0800

    add 0105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp 0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp

--- cpp/0105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
+++ cpp/0105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -32,16 +32,42 @@ Return the following binary tree:
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        
+        if(preorder.empty() || inorder.empty() && preorder.size() != inorder.size())
+            return nullptr;
+        return helper(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
     }
 
 private:
-    TreeNode* helper(){
+    TreeNode* helper(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd){
+        int rootVal = preorder[preStart];
+        TreeNode* root = new TreeNode(rootVal);
+
+        int i = inStart;
+        for(; i <= inEnd; i++)
+            if(inorder[i] == rootVal)
+                break;
+
+        // if(i > inEnd)
+        //     return nullptr;
+
+        int leftLen = i - inStart;
+        int rightLen = inEnd - i;
+
+        if(leftLen > 0)
+            root->left = helper(preorder, preStart + 1, preStart + leftLen, inorder, inStart, i - 1);
         
+        if(rightLen > 0)
+            root->right = helper(preorder, preStart + leftLen + 1, preEnd, inorder, i + 1, inEnd);
+
+        return root;
     }
 };
 
 
 int main(){
-
+    Solution S;
+    vector<int> preorder = {3,9,20,15,7};
+    vector<int> inorder = {9,3,15,20,7};
+    TreeNode* root = S.buildTree(preorder, inorder);
+    getchar();
 }
--- cpp/0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
+++ cpp/0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
@@ -32,10 +32,41 @@ Return the following binary tree:
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        
+        if(inorder.empty() || postorder.empty() && inorder.size() != postorder.size())
+            return nullptr;
+        return helper(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1);
+    }
+
+private:
+    TreeNode* helper(vector<int>& inorder, int inStart, int inEnd, vector<int>& postorder, int postStart, int postEnd){
+        int rootVal = postorder[postEnd];
+        TreeNode* root = new TreeNode(rootVal);
+
+        int i = inStart;
+        for(; i <= inEnd; i++)
+            if(inorder[i] == rootVal)
+                break;
+
+        // if(i > inEnd)
+        //     return nullptr;
+
+        int leftLen = i - inStart;
+        int rightLen =  inEnd - i;
+
+        if(leftLen)
+            root->left = helper(inorder, inStart, i - 1, postorder, postStart, postStart + leftLen - 1);
+
+        if(rightLen)
+            root->right = helper(inorder, i + 1, inEnd, postorder, postStart + leftLen, postEnd - 1);
+
+        return root;
     }
 };
 
 int main(){
-
+    Solution S;
+    vector<int> inorder = {9,3,15,20,7};
+    vector<int> postorder = {9,15,7,20,3};
+    TreeNode* root = S.buildTree(inorder, postorder);
+    getchar();
 }
