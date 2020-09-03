161ed84e673f63fb1fea47f8fde37dcb4d6c936b
zhangchi <zhangchi4@kingsoft.com>
2020-09-03 13:13:42 +0800

    add 0107_Binary_Tree_Level_Order_Traversal_II.cpp

--- cpp/0107_Binary_Tree_Level_Order_Traversal_II.cpp
+++ cpp/0107_Binary_Tree_Level_Order_Traversal_II.cpp
@@ -15,7 +15,6 @@ return its bottom-up level order traversal as:
   [9,20],
   [3]
 ]
-通过次数78,451
 */
 
 #include "common.h"
@@ -29,13 +28,49 @@ return its bottom-up level order traversal as:
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// 或者将结果保存在stack中,最后从stack弹出
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        
+        if(!root) return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        helper(q);
+        return res;
     }
+
+private:
+    void helper(queue<TreeNode*>& q){
+        if(q.empty())
+            return;
+        int count = q.size();
+        vector<int> nodes;
+        while(count--){
+            TreeNode* x = q.front();
+            q.pop();
+            nodes.push_back(x->val);
+            if(x->left)
+                q.push(x->left);
+
+            if(x->right)
+                q.push(x->right);
+        }
+
+        helper(q);
+        res.push_back(nodes);
+    }
+
+private:
+    vector<vector<int>> res;
 };
 
 int main(){
-
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+    Solution S;
+    printVectors(S.levelOrderBottom(root));
 }
