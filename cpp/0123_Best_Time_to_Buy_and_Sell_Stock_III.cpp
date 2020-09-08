2b821f12e76bbb1d3df7ce06d290617e45fb132d
zhangchi <zhangchi4@kingsoft.com>
2020-09-08 18:46:41 +0800

    0123_Best_Time_to_Buy_and_Sell_Stock_III.cpp

--- cpp/0123_Best_Time_to_Buy_and_Sell_Stock_III.cpp
+++ cpp/0123_Best_Time_to_Buy_and_Sell_Stock_III.cpp
@@ -42,10 +42,10 @@ Constraints:
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // dp[i][1][0] = max(dp[i-1][1][0], dp[i-1][1][1] + prices[i])
-        // dp[i][1][1] = max(dp[i-1][1][1], dp[i-1][0][0] - prices[i]) = max(dp[i-1][1][1], -prices[i])
         // dp[i][2][0] = max(dp[i-1][2][0], dp[i-1][2][1] + prices[i])
         // dp[i][2][1] = max(dp[i-1][2][1], dp[i-1][1][0] - prices[i])
+        // dp[i][1][0] = max(dp[i-1][1][0], dp[i-1][1][1] + prices[i])
+        // dp[i][1][1] = max(dp[i-1][1][1], dp[i-1][0][0] - prices[i]) = max(dp[i-1][1][1], -prices[i])
 
         // base case
         // dp[-1][k][0] = dp[i][0][0] = 0;
@@ -55,10 +55,10 @@ public:
         int dp_i20 = 0, dp_i21 = INT_MIN;
 
         for(auto price : prices){
-            dp_i10 = max(dp_i10, dp_i11 + price);
-            dp_i11 = max(dp_i11, -price);
             dp_i20 = max(dp_i20, dp_i21 + price);
             dp_i21 = max(dp_i21, dp_i10 - price);
+            dp_i10 = max(dp_i10, dp_i11 + price);
+            dp_i11 = max(dp_i11, -price);
         }
 
         return dp_i20;
