1f536bbb28aca5e52cf9ff2cc0a4defe391d2020
张驰 <zhangchi@zhangchideMacBook-Pro.local>
2020-09-10 01:11:53 +0800

    0131_Palindrome_Partitioning.cpp

--- cpp/0131_Palindrome_Partitioning.cpp
+++ cpp/0131_Palindrome_Partitioning.cpp
@@ -19,10 +19,51 @@ Output:
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-
+        len = s.size();
+        dp = vector<vector<bool>>(len,vector<bool>(len + 1, false));
+        backtrack(s, 0);
+        return res;
     }
+
+private:
+    void backtrack(const string& s, int start){
+        if(start == len){
+            res.push_back(path);
+            return;
+        }
+
+        for(int i = 1; i <= len - start; i++){
+            string subStr = s.substr(start, i);
+            if(dp[start][i] || isPalindrome(subStr)){
+                dp[start][i] = true;
+                path.push_back(subStr);
+                backtrack(s, start + i);
+                path.pop_back();
+            }
+        }
+    }
+
+    bool isPalindrome(const string& s){
+        int left = 0, right = s.size() - 1;
+        while(left < right){
+            if(s[left] != s[right])
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+private:
+    int len = 0;
+    vector<vector<string>> res;
+    vector<string> path;
+    vector<vector<bool>> dp;
 };
 
 int main(){
-
+    Solution S;
+    string s = "aab";
+    vector<vector<string>> res = S.partition(s);
+    printVectors(res);
 }
