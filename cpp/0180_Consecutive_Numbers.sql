9551a4aa956c0e77d3c1991fc94dd69f5b845cf7
zhangchi <zhangchi4@kingsoft.com>
2020-09-16 20:30:59 +0800

    init

---
+++ cpp/0180_Consecutive_Numbers.sql
@@ -0,0 +1,37 @@
+/*
+180. Consecutive Numbers
+SQL架构
+Write a SQL query to find all numbers that appear at least three times consecutively.
+
++----+-----+
+| Id | Num |
++----+-----+
+| 1  |  1  |
+| 2  |  1  |
+| 3  |  1  |
+| 4  |  2  |
+| 5  |  1  |
+| 6  |  2  |
+| 7  |  2  |
++----+-----+
+For example, given the above Logs table, 1 is the only number that appears consecutively for at least three times.
+
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+*/
+
+#include "common.h"
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+    }
+};
+
+int main(){
+
+}
