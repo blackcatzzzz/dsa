e170104d9e6307adabdf74f2f9ed88fd03f76215
zhangchi <zhangchi4@kingsoft.com>
2020-10-09 11:37:14 +0800

    0240_Search_a_2D_Matrix_IIcopy.cpp

--- cpp/0240_Search_a_2D_Matrix_IIcopy.cpp
+++ cpp/0240_Search_a_2D_Matrix_IIcopy.cpp
@@ -25,11 +25,26 @@ Given target = 20, return false.
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
+        if(matrix.empty() || matrix[0].empty())
+            return false;
+        int row = matrix.size() - 1;
+        int col = 0;
+        while(row >= 0 && col <= matrix[0].size() - 1){
+            if(target == matrix[row][col])
+                return true;
+            else if(target < matrix[row][col])
+                row--;
+            else
+                col++;
+        }
+
+        return false;
     }
 };
 
 
 int main(){
-
+    Solution S;
+    vector<vector<int>> matrix;
+    cout << S.searchMatrix(matrix, 1);
 }
