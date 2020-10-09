fdbe3e05c09cfe08f0c31040c83c20fe81efe8cb
zhangchi <zhangchi4@kingsoft.com>
2020-10-09 11:37:40 +0800

    0238_Product_of_Array_Except_Self.cpp

--- cpp/0238_Product_of_Array_Except_Self.cpp
+++ cpp/0238_Product_of_Array_Except_Self.cpp
@@ -19,11 +19,47 @@ Could you solve it with constant space complexity? (The output array does not co
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        vector<int> L(n, 0);
+        L[0] = 1;
+        for(int i = 1; i < n; i++)
+            L[i] = L[i - 1] * nums[i - 1];
 
+        int R = 1;
+        for(int i = n - 1; i >= 0; i--){
+            ans[i] = L[i] * R;
+            R *= nums[i];
+        }
+            
+        return ans;
+    }
+};
+
+class Solution_O_N {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        vector<int> L(n, 0), R(n, 0);
+        L[0] = 1;
+        for(int i = 1; i < n; i++)
+            L[i] = L[i - 1] * nums[i - 1];
+
+        R[n - 1] = 1;
+        for(int i = n - 2; i >= 0; i--)
+            R[i] = R[i + 1] * nums[i + 1];
+
+        for(int i = 0; i < n; i++)
+            ans[i] = L[i] * R[i];
+
+        return ans;
     }
 };
 
 
 int main(){
-
+    Solution S;
+    vector<int> nums = {1,2,3,4};
+    printVector(S.productExceptSelf(nums));
 }
