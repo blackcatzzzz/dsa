b0c8b970753a4b8674da4c0a7d1d807015269e54
zhangchi <zhangchi4@kingsoft.com>
2020-10-09 15:49:04 +0800

    0241_Different_Ways_to_Add_Parentheses.cpp

--- cpp/0241_Different_Ways_to_Add_Parentheses.cpp
+++ cpp/0241_Different_Ways_to_Add_Parentheses.cpp
@@ -26,11 +26,54 @@ Explanation:
 class Solution {
 public:
     vector<int> diffWaysToCompute(string input) {
-
+        return helper(input, 0, input.size() - 1);
     }
+
+private:
+    vector<int> helper(const string& input, int start, int end){
+        if(isAllNum(input, start, end))
+            return vector<int>{stoi(input.substr(start, end - start + 1))};
+        
+        if(memo.find(make_pair(start, end)) != memo.end())
+            return memo[make_pair(start, end)];
+        vector<int> ans;
+        for(int i = start; i <= end; i++){
+            char c = input[i];
+            if(c == '+' || c == '-' || c == '*'){
+                vector<int> left = helper(input, start, i - 1);
+                vector<int> right = helper(input, i + 1, end);
+                for(auto& l : left){
+                    for(auto& r: right){
+                        if(c == '+')
+                            ans.push_back(l + r);
+                        else if(c == '-')
+                            ans.push_back(l - r);
+                        else
+                            ans.push_back(l * r);
+                    }
+                }
+            }
+        }
+
+        memo[make_pair(start, end)] = ans;
+        
+        return ans;
+    }
+
+    bool isAllNum(const string& str, int start, int end){
+        for(int i = start; i <= end; i++)
+            if(!isdigit(str[i]))
+                return false;
+        return true;
+    }
+
+private:
+    map<pair<int, int>, vector<int>> memo;
 };
 
 
 int main(){
-
+    Solution S;
+    string input = "2*3-4*5";
+    printVector(S.diffWaysToCompute(input));
 }
