d1240171e2ef11e87a51c93c3089d651c9a24b7f
zhangchi <zhangchi4@kingsoft.com>
2020-10-16 14:33:11 +0800

    0021_MergeTwoSortedLists.cpp

--- cpp/0021_MergeTwoSortedLists.cpp
+++ cpp/0021_MergeTwoSortedLists.cpp
@@ -20,7 +20,36 @@ Output: 1->1->2->3->4->4
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* tail = dummy;
+        while(l1 && l2){
+            if(l1->val <= l2->val){
+                tail->next = l1;
+                l1 = l1->next;
+            }else{
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        if(l1)
+            tail->next = l1;
+
+        if(l2)
+            tail->next = l2;
+
+        return dummy->next;
+    }
+};
+
+class Solution1 {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         // if(!l1 && !l2) return nullptr;
