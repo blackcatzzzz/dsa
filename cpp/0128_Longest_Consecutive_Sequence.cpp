fcddcd84255f39b4a58071439b7b571af3fd8fe3
zhangchi <zhangchi4@kingsoft.com>
2020-10-21 17:07:33 +0800

    0128_Longest_Consecutive_Sequence.cpp

--- cpp/0128_Longest_Consecutive_Sequence.cpp
+++ cpp/0128_Longest_Consecutive_Sequence.cpp
@@ -13,10 +13,80 @@ Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefor
 
 #include "common.h"
 
+// union find set O(N)
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        if(nums.empty()) 
+            return 0;
 
+        for(auto& num : nums){
+            uf[num] = num;
+            cnt[num] = 1;
+        }
+            
+        int ans = 1;
+        for(auto& num : nums){
+            if(num != INT_MAX && uf.count(num + 1))
+                ans = max(ans, unionSet(num , num + 1));
+        }
+
+        return ans;
+    }
+
+private:
+    unordered_map<int, int> uf, cnt;
+
+    int find(int p){
+        int root = p;
+        while(root != uf[root])
+            root = uf[root];
+
+        while(uf[p] != p){
+            int cur = p;
+            p = uf[p];
+            uf[cur] = root;
+        }
+
+        return root;
+    }
+
+    int unionSet(int p, int q){
+        int rootP = find(p);
+        int rootQ = find(q);
+
+        if(rootP == rootQ)
+            return cnt[rootP];
+        
+        // p < q
+        uf[q] = rootP;
+        cnt[rootP] += cnt[rootQ];
+
+        return cnt[rootP];
+    }
+};
+
+// O(N)
+class Solution_Hash {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> nums_set;
+        for(auto& num : nums)
+            nums_set.insert(num);
+
+        int ans = 0;
+        for(auto& num : nums){
+            if(nums_set.count(num - 1))
+                continue;
+            
+            int r = num;
+            while(nums_set.count(r + 1))
+                r++;
+
+            ans = max(ans, r - num + 1);
+        }
+
+        return ans;
     }
 };
 
