23b44630771a45edd2dbc735bd8a19f15ee1dd48
zhangchi <zhangchi4@kingsoft.com>
2020-11-02 13:21:47 +0800

    0321_Create_Maximum_Number.cpp

--- cpp/0321_Create_Maximum_Number.cpp
+++ cpp/0321_Create_Maximum_Number.cpp
@@ -35,10 +35,89 @@ Output:
 class Solution {
 public:
     vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<int> ans;
+        for(int i = max(0, k - n); i <= min(m, k); i++){
+            vector<int> nums1_seq = maxSequence(nums1, i);
+            vector<int> nums2_seq = maxSequence(nums2, k - i);
+            vector<int> merged_seq = merge(nums1_seq, nums2_seq);
+            ans = compare(ans, merged_seq, 0, 0) ? ans : merged_seq;
+        }
 
+        return ans;
+    }
+
+private:
+    vector<int> maxSequence(vector<int>& nums, int k){
+        if(k <= 0)
+            return {};
+        stack<int> stk;
+        int n = nums.size() - k;
+        for(int i = 0; i < nums.size(); i++){
+            while(!stk.empty() && stk.top() < nums[i] && n-- > 0){
+                stk.pop();
+            }
+            stk.push(nums[i]);
+        }
+
+        while(stk.size() > k)
+            stk.pop();
+        vector<int> res(k, 0);
+        int pos = k - 1;
+        while(!stk.empty()){
+            res[pos--] = stk.top();
+            stk.pop();
+        }
+
+        return res;
+    }
+
+    vector<int> merge(vector<int>& nums1, vector<int>& nums2){
+        vector<int> res;
+        int i = 0, j = 0;
+        while(i < nums1.size() && j < nums2.size()){
+            if(nums1[i] > nums2[j])
+                res.push_back(nums1[i++]);
+            else if(nums1[i] < nums2[j])
+                res.push_back(nums2[j++]);
+            else{
+                if(compare(nums1, nums2, i + 1, j + 1))
+                    res.push_back(nums1[i++]);
+                else
+                    res.push_back(nums2[j++]);
+            }
+
+        }
+
+        while(i < nums1.size())
+            res.push_back(nums1[i++]);
+
+        while(j < nums2.size())
+            res.push_back(nums2[j++]);
+
+        return res;
+    }
+
+    // if nums1 >= nums2 return true (nums1.size() == nums2.size())
+    bool compare(vector<int> nums1, vector<int> nums2, int i, int j){
+        for(;i < nums1.size() && j < nums2.size(); i++, j++){
+            if(nums1[i] == nums2[j])
+                continue;
+
+            if(nums1[i] > nums2[j])
+                return true;
+            else
+                return false;      
+        }
+
+        return j == nums2.size();
     }
 };
 
 int main(){
-
+    vector<int> nums1 = {2,5,6,4,4,0};
+    vector<int> nums2 = {7,3,8,0,6,5,7,6,2};
+    Solution S;
+    printVector(S.maxNumber(nums1, nums2, 15));
 }
