ca824716c1718f1c399c92e84c2cabd32f979d86
zhangchi <zhangchi4@kingsoft.com>
2020-11-16 11:39:59 +0800

    0051_N-Queens.cpp 0052_N-Queens_II.cpp

--- cpp/0051_N-Queens.cpp
+++ cpp/0051_N-Queens.cpp
@@ -30,8 +30,49 @@ Explanation: There exist two distinct solutions to the 4-queens puzzle as shown 
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-
+        vector<string> board(n, string(n, '.'));
+        backtrace(board, 0);
+        return ans;
     }
+
+private:
+    bool isValid(vector<string>& board, int row, int col){
+        for(int i = 0; i < row; i++){
+            if(board[i][col] == 'Q')
+                return false;
+        }
+
+        for(int i = row - 1, j = col - 1; i >=0 && j >= 0; i--, j--){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+
+        for(int i = row - 1, j = col + 1; i >= 0 && j < board.size(); i--, j++){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    void backtrace(vector<string>& board, int row){
+        if(row == board.size()){
+            ans.push_back(board);
+            return;
+        }
+
+        for(int j = 0; j < board.size(); j++){
+            if(!isValid(board, row, j))
+                continue;
+
+            board[row][j] = 'Q';
+            backtrace(board, row + 1);
+            board[row][j] = '.';
+        }
+    }
+
+private:
+    vector<vector<string>> ans;
 };
 
 int main(){
--- cpp/0052_N-Queens_II.cpp
+++ cpp/0052_N-Queens_II.cpp
@@ -29,8 +29,50 @@ Explanation: There are two distinct solutions to the 4-queens puzzle as shown be
 class Solution {
 public:
     int totalNQueens(int n) {
-        
+        ans = 0;
+        vector<string> board(n, string(n, '.'));
+        backtrace(board, 0);
+        return ans;
     }
+
+private:
+    bool isValid(vector<string>& board, int row, int col){
+        for(int i = 0; i < row; i++){
+            if(board[i][col] == 'Q')
+                return false;
+        }
+
+        for(int i = row - 1, j = col - 1; i >=0 && j >= 0; i--, j--){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+
+        for(int i = row - 1, j = col + 1; i >= 0 && j < board.size(); i--, j++){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    void backtrace(vector<string>& board, int row){
+        if(row == board.size()){
+            ans++;
+            return;
+        }
+
+        for(int j = 0; j < board.size(); j++){
+            if(!isValid(board, row, j))
+                continue;
+
+            board[row][j] = 'Q';
+            backtrace(board, row + 1);
+            board[row][j] = '.';
+        }
+    }
+
+private:
+    int ans;
 };
 
 int main(){
