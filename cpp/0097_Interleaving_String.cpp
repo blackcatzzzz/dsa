ea0978c0f3bd5b3295815d19e28f344e13d0c18c
zhangchi <zhangchi4@kingsoft.com>
2020-11-18 14:52:53 +0800

    0097_Interleaving_String.cpp

--- cpp/0097_Interleaving_String.cpp
+++ cpp/0097_Interleaving_String.cpp
@@ -41,7 +41,30 @@ s1, s2, and s3 consist of lower-case English letters.
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
+        return helper(s1, s2, s3);
+    }
 
+private:
+    bool helper(const string& s1, const string & s2, const string& s3){
+        int len1 = s1.size();
+        int len2 = s2.size();
+        if(len1 + len2 != s3.size())
+            return false;
+
+        vector<vector<bool>> dp(len1 + 1, vector<bool>(len2 + 1, false));
+        dp[0][0] = true;
+
+        for(int i = 0; i <= len1; i++){
+            for(int j = 0; j <= len2; j++){
+                if(i > 0)
+                    dp[i][j] = s1[i - 1] == s3[i + j -1] && dp[i - 1][j];
+
+                if(j > 0)
+                    dp[i][j] = dp[i][j] || (s2[j - 1] == s3[i + j -1] && dp[i][j - 1]);
+            }
+        }
+
+        return dp[len1][len2];
     }
 };
 
