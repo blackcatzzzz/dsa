65255a6b3084ecb65f75574301e0ccbc0b7c0598
zhangchi <zhangchi4@kingsoft.com>
2020-11-27 19:16:12 +0800

    0126_Word_Ladder_II.cpp

--- cpp/0126_Word_Ladder_II.cpp
+++ cpp/0126_Word_Ladder_II.cpp
@@ -40,10 +40,113 @@ Explanation: The endWord "cog" is not in wordList, therefore no possible transfo
 class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        vector<string> path;
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> allComboDict;
 
+        int len = beginWord.size();
+
+        for(auto & word: wordList){
+            for(int i = 0; i < len; i++){
+                string newWord = word.substr(0, i) + "*" + word.substr(i + 1);
+                allComboDict[newWord].push_back(word);
+            }
+        }
+
+        unordered_map<string, bool> visited;
+        queue<pair<string, vector<string>>> q;
+        bool find = false;
+
+        q.push(make_pair(beginWord, vector<string>(1, beginWord)));
+        while(!q.empty() && !find){
+            int size = q.size();
+            while(size--){
+                auto node = q.front();
+                q.pop();
+                
+                for(int i = 0; i < len; i++){
+                    string newWord = node.first.substr(0, i) + "*" + node.first.substr(i + 1);
+                    vector<string> adjacentWords = allComboDict[newWord];
+                    for(auto& adjacentWord : adjacentWords){
+                        if(visited[adjacentWord])
+                            continue;
+                        
+                        vector<string> path(node.second);
+                        path.push_back(adjacentWord);
+                        if(adjacentWord == endWord){
+                            ans.push_back(path);
+                            find = true;
+                            continue;
+                        }
+
+                        visited[adjacentWord] = true;
+                        q.push(make_pair(adjacentWord, path));
+                    }
+                }
+            }
+        }
+
+        return ans;
     }
+
+};
+
+// find all
+class Solution_All {
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        _len = beginWord.size();
+        _endWord = endWord;
+
+        for(auto & word: wordList){
+            for(int i = 0; i < _len; i++){
+                string newWord = word.substr(0, i) + "*" + word.substr(i + 1);
+                allComboDict[newWord].push_back(word);
+            }
+        }
+
+        path.push_back(beginWord);
+        dfs(beginWord);
+
+        return ans;
+    }
+
+private:
+    void dfs(const string& beginWord){
+        if(beginWord == _endWord){
+            ans.push_back(path);
+            return;
+        }
+
+        for(int i = 0; i < _len; i++){
+            string newWord = beginWord.substr(0, i) + "*" + beginWord.substr(i + 1);
+            vector<string> adjacentWords = allComboDict[newWord];
+            for(auto& adjacentWord : adjacentWords){
+                if(visited[adjacentWord])
+                    continue;
+
+                path.push_back(adjacentWord);
+                visited[adjacentWord] = true;
+                dfs(adjacentWord);
+                visited[adjacentWord] = false;
+                path.pop_back();
+            }
+        }
+    }
+
+private:
+    int _len;
+    string _endWord;
+    vector<string> path;
+    vector<vector<string>> ans;
+    unordered_map<string, vector<string>> allComboDict;
+    unordered_map<string, bool> visited;
 };
 
 int main(){
-
+    string beginWord = "hit";
+    string endWord = "cog";
+    vector<string> wordList = {"hot","dot","dog","lot","log","cog"};
+    Solution S;
+    printVectors(S.findLadders(beginWord, endWord, wordList));
 }
