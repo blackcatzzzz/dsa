9f667f445fd39e60421a6e929ad6314dfff0e31d
zhangchi <zhangchi4@kingsoft.com>
2021-07-12 11:31:51 +0800

    update 153

--- cpp/0153_Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ cpp/0153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -20,7 +20,28 @@ Output: 0
 
 #include "common.h"
 
+
 class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        if(nums.size() == 0)
+            return -1;
+        
+        int left = 0, right = nums.size() - 1;
+
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > nums[right])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        return nums[left];
+    }
+};
+
+class Solution1 {
 public:
     int findMin(vector<int>& nums) {
         if(nums.size() == 0)
